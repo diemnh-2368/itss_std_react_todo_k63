14d764dd2b46381ae638bd6e767c3a4d6f7a0f2e
EC2 Default User <ec2-user@ip-172-31-20-246.ec2.internal>
2021-05-20 08:51:36 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAU4T7XoRZJS_NGyPfYn1FoMDeN3I3PvGw",
+    authDomain: "fir-sample-faabb.firebaseapp.com",
+    projectId: "fir-sample-faabb",
+    storageBucket: "fir-sample-faabb.appspot.com",
+    messagingSenderId: "826639031334",
+    appId: "1:826639031334:web:7a39eae2650b2456da9ae7"
+};
+firebase.initializeApp(firebaseConfig);
