802d9a6c845fbca2a67a4394a9e49035b4c63c48
EC2 Default User <ec2-user@ip-172-31-9-71.ec2.internal>
2021-05-21 08:34:20 +0000

    Use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVKKaEkPZhHaQVexue5dtB9uNEYRMk6yU",
+  authDomain: "ardent-fastness-301013.firebaseapp.com",
+  projectId: "ardent-fastness-301013",
+  storageBucket: "ardent-fastness-301013.appspot.com",
+  messagingSenderId: "983426799628",
+  appId: "1:983426799628:web:db138967f4e35f84dfb629"
+};
+
+firebase.initializeApp(firebaseConfig);
