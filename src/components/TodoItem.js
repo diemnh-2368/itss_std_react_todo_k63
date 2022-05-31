b16021881d69da1f1017bc0e249571f426a22040
longpt-4141 <pthanhlong259@gmail.com>
2022-05-31 09:29:24 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import '../styles/check.css';
 /* 
   【TodoItemコンポーネント】
 　・Todoアイテムを表示する
@@ -5,10 +7,15 @@
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
 function TodoItem(props) {
+  const [checked, setChecked] = React.useState(props.item.done);
+  const handleCheckBox = () => {
+    console.log('Long');
+    setChecked(!checked);
+  }
   return (
     <label className="panel-block">
-      <input type="checkbox" />
-            {props.item.text}
+      <input type="checkbox" onClick={handleCheckBox} />
+            <p className={checked ? 'has-text-grey-light' : 'normal'}>{props.item.text}</p>
     </label>
   );
 }
