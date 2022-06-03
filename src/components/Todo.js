44a4e9575874e7d33736842e4c7b16701ba443db
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2022-06-03 13:15:59 +0700

    Add new todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 /* 
   【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
+ ・key：Todoを特定するID（String）
+ ・text：Todoの内容（String）
+ ・done：完了状態（Boolean true:完了済み,, false:未完了）
 */
 
 /* コンポーネント */
@@ -16,24 +16,52 @@ import Filter from './Filter';
 import useStorage from '../hooks/storage';
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
   const [items, putItems] = React.useState([
-      /* テストコード 開始 */
+    /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
     { key: getKey(), text: 'reactを勉強する', done: false },
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
 
+  const [name, setName] = useState('');
+  
+  const onChangeText = (e) => {
+    setName(e.target.value);
+
+    // User press Enter
+    if (e.key === 'Enter') {
+      // e.preventDefault();
+      // e.stopImmediatePropagation();
+      const newTodo = {
+        key: getKey(),
+        text: name,
+        done: false
+      }
+      putItems([...items, newTodo]);
+      setName('');
+    }
+  }
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
+
+      <input
+        className='input'
+        type='text'
+        onKeyDown={onChangeText}
+        onChange={onChangeText}
+        placeholder='Todoを入力'
+        value={name}
+      />
       {items.map(item => (
-        <TodoItem 
+        <TodoItem
           key={item.key}
           text={item.text}
         />
