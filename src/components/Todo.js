efc929a57e5cb51498d9a33502a0f41a3041a618
dangcuong-123 <dangcuonga2songda@gmail.com>
2022-06-05 20:02:54 +0000

    save todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,14 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
   const [filter, setFilter] = React.useState('ALL');
 
   const displayItems = items.filter(item => {
@@ -52,7 +53,7 @@ function Todo() {
   
   return (
     <div className="panel">
-      <div className="panel-heading">
+      <div className="panel-heading has-background-success">
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
@@ -69,6 +70,11 @@ function Todo() {
       ))}
       <div className="panel-block">
         {displayItems.length} items
+      </div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth has-background-success" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
     </div>
   );
