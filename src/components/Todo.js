ac37599b89368b2e6012636e348b04bd3d226b72
dattrinh09 <tiendattrinh09@gmail.com>
2022-06-05 17:10:02 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,24 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+
+  const [content, setContent] = useState('');
+  const [tabSelected, setTabSelected] = useState(0);
+  const [items, putItems, clearItems] = useStorage();
+
+  const tabs = ['全て', '未完了', '完了済み'];
+
+  const onSubmit = () => {
+    putItems([...items, { key: getKey(), text: content, done: false }]);
+    setContent('');
+  }
 
   const onUpdate = (data) => {
     const index = items.findIndex(item => item.key === data.key);
@@ -36,20 +47,6 @@ function Todo() {
     }
   };
 
-  const [content, setContent] = useState('');
-  const [tabSelected, setTabSelected] = useState(0);
-
-  useEffect(() => {
-    
-  }, []);
-
-  const tabs = ['全て', '未完了', '完了済み'];
-
-  const onSubmit = () => {
-    putItems([...items, { key: getKey(), text: content, done: false }]);
-    setContent('');
-  }
-
   const getLists = () => {
     switch(tabSelected){
       case 0: return items;
@@ -59,9 +56,9 @@ function Todo() {
     }
   }
 
-  //  const onRemove = () => {
-  //   clearItems();
-  // } 
+  const onRemove = () => {
+    clearItems();
+  } 
 
   return (
     <div className="panel">
@@ -71,10 +68,12 @@ function Todo() {
       <input type='text'
         value={content}
         onChange={e => setContent(e.target.value)}
+        placeholder='Todoを入力してください'
         onKeyUp={e => {
           if(e.key === 'Enter')
             onSubmit()
         }}/>
+      
       <div className='tabs'>
         {
           tabs.map((tab, index) => (<div className={`tab ${index === tabSelected ? 'active' : ''}`} key={index} onClick={() => setTabSelected(index)} >{tab}</div>))
@@ -90,6 +89,13 @@ function Todo() {
       <div className="panel-block">
         {items.length} items
       </div>
+
+      <input
+        type='submit'
+        value='全てTodoを削除'
+        onClick={onRemove}
+      />
+      
     </div>
   );
 }
