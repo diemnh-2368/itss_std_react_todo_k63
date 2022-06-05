e30859306c95013df811dcf5a2935bb781dda78a
Truong <gnourt2112000@gmail.com>
2022-06-05 12:09:02 +0700

    Add tabs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,9 +4,44 @@
 　・タブで表示する
 　・サポートするステータスは「すべて」「未完了」「完了済み」
 */
-function Filter(  ) {
+import {useState} from "react";
+
+const status = [
+  {
+    name:"すべて",
+    active:true,
+  },
+  {
+    name:"未完了",
+    active:false,
+  },
+  {
+    name:"完了済み",
+    active:false,
+  }
+
+];
+
+
+function Filter({filter}) {
+  const [filterstatus,setFilterstatus] = useState(status)
+
+  const onToggleStatus = (val) =>{
+    setFilterstatus(filterstatus.map(element => {
+      if(element.name === val.name){
+        element.active =true
+      }else{
+        element.active =false
+      }
+      return element;
+    }))
+  }
+
   return (
     <div className="panel-tabs">
+      {filterstatus.map(element => 
+        <a className={element.active ? "is-active" :""} onClick={() => {onToggleStatus(element);filter(element.name)}}>{element.name}</a>  
+      )}
     </div>
   );
 }
