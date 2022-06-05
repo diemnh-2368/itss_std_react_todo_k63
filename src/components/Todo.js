0451577b57cb40f89d5c2095fef12abab80688dd
Truong <gnourt2112000@gmail.com>
2022-06-05 11:43:44 +0700

    Add new todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -38,11 +38,16 @@ function Todo() {
     putItems(newItems);
   }
 
+  const addTodo = (item) =>{
+    putItems([...items,item])
+  }
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
+      <Input addTodo={addTodo}/>
       {items.map(item => (
         <TodoItem item={item} handleDone={handleDone}/>
       ))}
