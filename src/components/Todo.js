bf74304e205a40e024b2e9e37e4c4a6131fa6a4f
duylong <hoangduylong2100@gmail.com>
2022-06-09 08:05:13 +0000

    almost complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 /* 
   【Todoのデータ構成】
@@ -19,27 +19,59 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: true },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+
+  const [displayItems, setDisplayItems] = useState(items)
+  // 0: 全て, 1: 未完了, 2: 完了済み
+  const [optionFilter, setOptionFilter] = useState(0)
+
+  const addItem = (text) => {
+    let newItem = {key: getKey(), text, done: false}
+    putItems([...items, newItem])
+  }
+
+  useEffect(() => {
+    let newDisplayItems = items.filter(x => {
+      if (optionFilter == 0) {
+        return true
+      }
+      if (optionFilter == 1) {
+        return !x.done
+      }
+
+      return x.done
+    })
+
+    setDisplayItems(newDisplayItems)
+  }, [items, optionFilter])
 
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      {items.map(item => (
-        <TodoItem
-          key={item.key}
-          item={item}
-        />
+
+      <Input setData={(text) => addItem(text)} />
+
+      <Filter option={optionFilter} setOption={setOptionFilter} />
+
+      {displayItems.map(item => (
+        <TodoItem key={item.key} item={item} data={items} setData={putItems} />
       ))}
+
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
+      </div>
+
+      <div className='panel-block'>
+        <button className='button is-fullwidth' onClick={() => clearItems()}>全てのToDoを削除</button>
       </div>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,30 @@
+import { useState } from "react"
+
 /* 
   【TodoItemコンポーネント】
 　・Todoアイテムを表示する
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem({item}) {
+function TodoItem({ item, data, setData }) {
+  const [styled, setStyle] = useState(item.done)
+
+  const changeStyle = () => {
+    let newData = []
+    data.forEach(e => {
+      let newElement = e
+      if (e.key === item.key) {
+        newElement.done = !newElement.done
+      }
+      newData.push(newElement)
+    });
+    setData(newData)
+    setStyle(!styled)
+  }
+
   return (
-    <label className="panel-block">
-      <input type="checkbox" />
+    <label className={(styled? "has-text-grey-light": "") + " panel-block"} >
+      <input defaultChecked={styled} type="checkbox" onChange={() => changeStyle()} />
       {item.text}
     </label>
   );
