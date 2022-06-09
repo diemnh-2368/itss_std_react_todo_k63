8f3e0e61409a3bc3c1bbcd1142bf0f85f9b262e4
thien <thuanthiennguyen171@gmail.com>
2022-06-09 05:30:20 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,28 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  const handleFilterChange = value => setFilter(value);
+
+  const handleAdd = text => {
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
+
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -35,24 +50,30 @@ function Todo() {
     });
     putItems(newItems);
   };
-  const handleAdd = text => {
-    putItems([...items, { key: getKey(), text, done: false }]);
-  };
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
-      {items.map(item => (
-         <TodoItem 
-              key={(item.key)}
-              item={item}
-              onCheck={handleCheck}
-            />
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
+        <TodoItem
+          key={item.key}
+          item={item}
+          onCheck={handleCheck}
+        />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
+      </div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button> 
       </div>
     </div>
   );
